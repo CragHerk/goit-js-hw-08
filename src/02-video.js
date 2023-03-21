7c79b19c0709e92e859e4a87af77af40506ef862
m.backi <backimichal@gmail.com>
2023-03-21 16:23:01 +0100

    2
    2

---
+++ src/02-video.js
@@ -0,0 +1,28 @@
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
+
+const iframe = document.querySelector("#vimeo-player");
+const player = new Player(iframe);
+
+const currentTime = localStorage.getItem("videoplayer-current-time");
+if (currentTime) {
+  const parsedTime = parseInt(currentTime, 10); // parsujemy string do liczby
+  if (parsedTime >= 0 && parsedTime < player.duration) {
+    // sprawdzamy czy czas jest poprawny
+    player.setCurrentTime(parsedTime);
+  }
+}
+
+player.on(
+  "timeupdate",
+  throttle(() => {
+    const currentTime = Math.round(player.currentTime);
+    if (
+      !isNaN(currentTime) &&
+      currentTime >= 0 &&
+      currentTime <= player.duration
+    ) {
+      localStorage.setItem("videoplayer-current-time", currentTime);
+    }
+  }, 1000)
+);
---
+++ src/03-feedback.js
@@ -0,0 +1,36 @@
+import throttle from "lodash.throttle";
+
+const form = document.querySelector(".feedback-form");
+const emailInput = form.querySelector('input[type="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+
+// Podczas ładowania strony sprawdzamy, czy w local storage są zapisane dane formularza.
+// Jeśli tak, to wypełniamy nimi pola formularza.
+const state = JSON.parse(localStorage.getItem("feedback-form-state")) || {};
+emailInput.value = state.email || "";
+messageInput.value = state.message || "";
+
+// Śledzimy zdarzenie input w formularzu i zapisujemy aktualny stan formularza w local storage.
+form.addEventListener(
+  "input",
+  throttle(() => {
+    const state = {
+      email: emailInput.value,
+      message: messageInput.value,
+    };
+    localStorage.setItem("feedback-form-state", JSON.stringify(state));
+  }, 500)
+);
+
+// Wysyłamy dane formularza i czyszczymy local storage oraz pola formularza.
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const state = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  console.log(state);
+  localStorage.removeItem("feedback-form-state");
+  emailInput.value = "";
+  messageInput.value = "";
+});
