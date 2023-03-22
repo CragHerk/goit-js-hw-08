d3f80fe244b1d9d3ed7f75eba9a4f14d520b9d7c
CragHerk <108221259+CragHerk@users.noreply.github.com>
2023-03-22 12:31:58 +0100

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,9 +1,9 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const iframe = document.querySelector('#vimeo-player');
-const player = new Player(iframe);
+const iframe = document.getElementById('vimeo-player');
 
+const player = new Player(iframe);
 const currentTime = localStorage.getItem('videoplayer-current-time');
 if (currentTime) {
   player.setCurrentTime(currentTime);
