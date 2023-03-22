5ec011d654f95face82299ad345376a0434e187a
CragHerk <108221259+CragHerk@users.noreply.github.com>
2023-03-22 12:31:34 +0100

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,10 +1,10 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 import SimpleLightbox from 'simplelightbox';
-// Dodatkowy import stylów
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-// Change code below this line
+const gallery = document.querySelector(".gallery")
+
 const createItem = (item) => {
   return `
       <a class="gallery__item" href="${item.original}" data-autoshow="false">
