8c52597cbc731aa107aa43d7e7e0957218ca09f4
CragHerk <108221259+CragHerk@users.noreply.github.com>
2023-03-22 12:27:07 +0100

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,36 +1,32 @@
-import throttle from 'lodash.throttle';
+import throttle  from 'lodash.throttle';
 
-const form = document.querySelector('.feedback-form');
-const emailInput = form.querySelector('input[type="email"]');
-const messageInput = form.querySelector('textarea[name="message"]');
+const emailInput = document.querySelector('input[name="email"]');
+const messageInput = document.querySelector('textarea[name="message"]');
+const form = document.querySelector('.feedback-form')
 
-// Podczas ładowania strony sprawdzamy, czy w local storage są zapisane dane formularza.
-// Jeśli tak, to wypełniamy nimi pola formularza.
-const state = JSON.parse(localStorage.getItem('feedback-form-state')) || {};
-emailInput.value = state.email || '';
-messageInput.value = state.message || '';
+emailInput.addEventListener('input', throttle(saveStateToLocalStorage, 500));
+messageInput.addEventListener('input', throttle(saveStateToLocalStorage, 500));
 
-// Śledzimy zdarzenie input w formularzu i zapisujemy aktualny stan formularza w local storage.
-form.addEventListener(
-  'input',
-  throttle(() => {
-    const state = {
-      email: emailInput.value,
-      message: messageInput.value,
-    };
-    localStorage.setItem('feedback-form-state', JSON.stringify(state));
-  }, 500)
-);
-
-// Wysyłamy dane formularza i czyszczymy local storage oraz pola formularza.
-form.addEventListener('submit', event => {
-  event.preventDefault();
+window.addEventListener('load', () => {
+  const state = JSON.parse(localStorage.getItem('feedback-form-state')) || {};
+  emailInput.value = state.email || '';
+  messageInput.value = state.message || '';
+});
+function saveStateToLocalStorage() {
   const state = {
     email: emailInput.value,
     message: messageInput.value,
   };
-  console.log(state);
+  localStorage.setItem('feedback-form-state', JSON.stringify(state));
+}
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  console.log(
+    'Form submitted with state:',
+    JSON.stringify({ email: emailInput.value, message: messageInput.value })
+  );
   localStorage.removeItem('feedback-form-state');
   emailInput.value = '';
-  messageInput.value = '';
+  messageInput.value '';
+  
 });
